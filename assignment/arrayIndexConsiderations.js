e3166228dfa3111b49ee247320448faf3b23ca97
Ben Lauderbaugh <benjlaud1@gmail.com>
2017-03-15 22:59:41 -0500

    Completed arrayIndexConsiderations.js, arrays.js, booleanCondition.js, and doWhileLoops.js

--- assignment/arrayIndexConsiderations.js
+++ assignment/arrayIndexConsiderations.js
@@ -5,14 +5,16 @@ var colors = ['red', 'blue', 'yellow'];
 // using array indices, write an expression that will evaluate to the color described by these variable names
 // for example var theColorBlue = colors[1];
 // uncomment the next two lines and replace ____ with the appropriate code
-// var theColorRed = _____;
-// var theColorYellow = _____;
-
+var theColorRed = colors[0];
+var theColorYellow = colors[2];
 // what happens if we access an index that has no element?
 // uncomment the following line and replace ____ with the appropriate code
-// var fortyThirdColor = _____;
+var fortyThirdColor = colors[42]; // this variable will be undefined 3-15-17 BLL
 
 function lastItem( array ) {
   // this function accepts an array as an argument
   // have it return the last item in the array
+  return (array[array.length - 1]);
 }
+
+console.log(lastItem(colors));
--- assignment/booleanConditions.js
+++ assignment/booleanConditions.js
@@ -1,4 +1,12 @@
 function isCartonFull( eggsInCarton, cartonCapacity ){
   // write an if statement checks if the carton is at cartonCapacity
   // this function should return true if so, false if not
+  if (eggsInCarton === cartonCapacity) {
+    return true;
+  } else {
+    return false;
+  }
 } // end checkIfCartonIsFull
+
+console.log('Is this carton full?',isCartonFull(12,12));
+console.log('Is this carton full?',isCartonFull(10,12));
